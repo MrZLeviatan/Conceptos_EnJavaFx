81aeb8cc3e1f247a511fb6237ce8c69ce8b1cd47
Nicolas <117557162+MrZLeviatan@users.noreply.github.com>
2023-10-11 22:45:23 -0500

    Se crea el repositorio

---
+++ EjemplosJavaFx/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module mrzleviatan.ejemplosjavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens mrzleviatan.ejemplosjavafx to javafx.fxml;
+    exports mrzleviatan.ejemplosjavafx;
+}
